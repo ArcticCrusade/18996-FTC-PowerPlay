f55b83e40607966f688e4bc7cb0d9178597057f5
CMP-18996 <calculusgeek11@gmail.com>
2023-03-18 18:52:01 -0400

    redid step 5 tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0175;
-    public static double kA = 0.0025;
-    public static double kStatic = 0.02;
+    public static double kV = 0.0167;
+    public static double kA = 0.003;
+    public static double kStatic = 0.015;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
