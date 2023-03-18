985150d0876fc1a22c752dc9c4c4113253f3420f
christopheraustin <arcticplayernoice@gmail.com>
2023-03-17 21:46:15 -0400

    Cleaned up code and added Post Processing Phase 2 Notes: Post Processing still needs to be finished Untested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/OpenCVTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/OpenCVTesting.java
@@ -4,7 +4,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import java.util.ArrayList;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
@@ -68,6 +71,7 @@ public class OpenCVTesting extends LinearOpMode {
         int phase;
         private String viewedColor;
 
+
         public DetectionPipeline() {
             phase = 1;
         }
@@ -78,31 +82,58 @@ public class OpenCVTesting extends LinearOpMode {
 
         @Override
         public Mat processFrame(Mat input) {
-            if (phase == 1) {
-                double[] colorsSeen = input.get(120, 160);
-                int red = (int) colorsSeen[0];
-                int green = (int) colorsSeen[1];
-                int blue = (int) colorsSeen[2];
-                if (red > blue && red > green) {
-                    viewedColor = "RED";
-                } else if (blue > red && blue > green) {
-                    viewedColor = "BLUE";
-                } else if (green > red && green > blue) {
-                    viewedColor = "GREEN";
+            switch (phase) {
+                case 1: input = processPhase1(input);
+
+                case 2: input = processPhase2(input);
+            }
+            return input;
+        }
+
+        private Mat processPhase2(Mat input) {
+            Mat filteredImage = new Mat();
+            Imgproc.medianBlur(input, filteredImage, 21);
+            Core.extractChannel(filteredImage, filteredImage, 2);
+            Core.extractChannel(filteredImage, filteredImage, 1);
+            Imgproc.cvtColor(filteredImage, filteredImage, Imgproc.COLOR_GRAY2RGB);
+            Imgproc.threshold(filteredImage, filteredImage, 2, 29, Imgproc.THRESH_BINARY);
+            ArrayList<MatOfPoint> contours = new ArrayList<>();
+            Imgproc.findContours(filteredImage, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            double maxArea = 0;
+            int maxContourIndex = -1;
+            for (int i = 0; i < contours.size(); i++) {
+                double area = Imgproc.contourArea(contours.get(i));
+                if (area > maxArea) {
+                    maxArea = area;
+                    maxContourIndex = i;
                 }
-                Imgproc.rectangle(
-                        input,
-                        new Point(
-                                7 * input.cols() / 16,
-                                7 * input.rows() / 16),
-                        new Point(
-                                input.cols() * (9f / 16f),
-                                input.rows() * (9f / 16f)),
-                        new Scalar(red, green, blue), 4);
             }
-            else if (phase == 2) {
-                // crazy shit
+            Imgproc.drawContours(input, contours, maxContourIndex, new Scalar(255, 255, 0), 7);
+            return input;
+        }
+
+        private Mat processPhase1(Mat input) {
+            double[] colorsSeen = input.get(120, 160);
+            int red = (int) colorsSeen[0];
+            int green = (int) colorsSeen[1];
+            int blue = (int) colorsSeen[2];
+            if (red > blue && red > green) {
+                viewedColor = "RED";
+            } else if (blue > red && blue > green) {
+                viewedColor = "BLUE";
+            } else if (green > red && green > blue) {
+                viewedColor = "GREEN";
             }
+            Imgproc.rectangle(
+                    input,
+                    new Point(
+                            7 * input.cols() / 16,
+                            7 * input.rows() / 16),
+                    new Point(
+                            input.cols() * (9f / 16f),
+                            input.rows() * (9f / 16f)),
+                    new Scalar(red, green, blue), 4);
             return input;
         }
 
