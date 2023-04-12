b7dc33a90ae3ca00a47a8156d6b3fe7093d78798
CMP-18996 <calculusgeek11@gmail.com>
2023-04-11 21:01:28 -0400

    auto now drops in right places

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RRPractical.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RRPractical.java
@@ -13,9 +13,9 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 @Config
 @Autonomous(name="RRPractical", group="Linear Opmode")
 public class RRPractical extends LinearOpMode {
-    public static double x = -33;
-    public static double y = 4;
-    public static double angle = 170;
+    public static double x = -28;
+    public static double y = -1;
+    public static double angle = -10;
     @Override
     public void runOpMode() {
         //yoink all of the motor declarations and their methods
@@ -33,17 +33,13 @@ public class RRPractical extends LinearOpMode {
                 .splineTo(new Vector2d(-32, 7.25), Math.toRadians(270))
                 .build();
         Trajectory toPosition = drive.trajectoryBuilder(firstToHigh.end(), true)
-                .splineTo(new Vector2d(-37, 10), Math.toRadians(270))
-                .splineToConstantHeading(new Vector2d(-43.5, 8), 0)
+                .splineToLinearHeading(new Pose2d(-43.5, 6, Math.toRadians(-10)), Math.toRadians(-10))
                 .build();
         Trajectory cycleFwd = drive.trajectoryBuilder(toPosition.end())
                 .splineTo(new Vector2d(x, y), Math.toRadians(angle))
                 .build();
         Trajectory cycleBk = drive.trajectoryBuilder(cycleFwd.end(), true)
-                .addDisplacementMarker(() -> {
-                    drive.operateClaw(1);
-                })
-                .splineTo(new Vector2d(-43.5, 8), Math.toRadians(170))
+                .splineToLinearHeading(new Pose2d(-43.5, 8, Math.toRadians(-10)), Math.toRadians(-10))
                 .build();
         waitForStart();
         if (isStopRequested()) return;
@@ -51,14 +47,14 @@ public class RRPractical extends LinearOpMode {
         drive.operateClaw(1);
         sleep(300);
         drive.followTrajectory(toPosition);
-        drive.turn(Math.toRadians(170));
         for (int i = 1; i <= 5; i++) {
             drive.operateClaw(0);
             drive.servosGoToPickup();
-            sleep(1300);
+            sleep(1400);
             drive.servosGoToUpright();
-            sleep(300);
+            sleep(450);
             drive.followTrajectory(cycleFwd);
+            drive.operateClaw(1);
             drive.followTrajectory(cycleBk);
             sleep(500);
         }
