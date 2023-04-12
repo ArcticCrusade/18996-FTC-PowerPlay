a62ae3678eaab3c82296e09647d5f3b19a3b552c
CMP-18996 <calculusgeek11@gmail.com>
2023-04-11 20:29:29 -0400

    checkpoint: +1 auto, still buggy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RRPractical.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RRPractical.java
@@ -13,35 +13,54 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 @Config
 @Autonomous(name="RRPractical", group="Linear Opmode")
 public class RRPractical extends LinearOpMode {
-    public static double x1 = 28;
-    public static double y1 = -5;
-    public static double angle = 104;
-    public static double x2 = 47;
-    public static double y2 = -12;
+    public static double x = -33;
+    public static double y = 4;
+    public static double angle = 170;
     @Override
     public void runOpMode() {
         //yoink all of the motor declarations and their methods
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         //set starting position
-        Pose2d startPose = new Pose2d(56.83, 34.4, 0);
+        Pose2d startPose = new Pose2d(-36, 72, Math.toRadians(270));
         drive.setPoseEstimate(startPose);
 
-        TrajectorySequence t = drive.trajectorySequenceBuilder(startPose)
+        TrajectorySequence firstToHigh = drive.trajectorySequenceBuilder(startPose)
                 .addDisplacementMarker(() -> {
                     drive.servosGoToUpright();
+                    drive.operateClaw(0);
                 })
-                .splineTo(new Vector2d(x1, y1), 0)
+                .splineTo(new Vector2d(-32, 7.25), Math.toRadians(270))
+                .build();
+        Trajectory toPosition = drive.trajectoryBuilder(firstToHigh.end(), true)
+                .splineTo(new Vector2d(-37, 10), Math.toRadians(270))
+                .splineToConstantHeading(new Vector2d(-43.5, 8), 0)
+                .build();
+        Trajectory cycleFwd = drive.trajectoryBuilder(toPosition.end())
+                .splineTo(new Vector2d(x, y), Math.toRadians(angle))
+                .build();
+        Trajectory cycleBk = drive.trajectoryBuilder(cycleFwd.end(), true)
                 .addDisplacementMarker(() -> {
                     drive.operateClaw(1);
                 })
-                .addDisplacementMarker(() -> {
-                    drive.servosGoToPickup();
-                })
-                .splineTo(new Vector2d(x2, y2), angle)
+                .splineTo(new Vector2d(-43.5, 8), Math.toRadians(170))
                 .build();
         waitForStart();
         if (isStopRequested()) return;
-        drive.followTrajectorySequence(t);
+        drive.followTrajectorySequence(firstToHigh);
+        drive.operateClaw(1);
+        sleep(300);
+        drive.followTrajectory(toPosition);
+        drive.turn(Math.toRadians(170));
+        for (int i = 1; i <= 5; i++) {
+            drive.operateClaw(0);
+            drive.servosGoToPickup();
+            sleep(1300);
+            drive.servosGoToUpright();
+            sleep(300);
+            drive.followTrajectory(cycleFwd);
+            drive.followTrajectory(cycleBk);
+            sleep(500);
+        }
     }
 }
