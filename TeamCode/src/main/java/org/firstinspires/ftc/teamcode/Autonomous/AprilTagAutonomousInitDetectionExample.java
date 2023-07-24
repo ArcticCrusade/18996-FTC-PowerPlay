2f6430aba8ea5e9c49f7866daa5c9a2c20b5f5e2
CMP-18996 <calculusgeek11@gmail.com>
2023-07-24 13:38:15 -0400

    fixed some things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AprilTagAutonomousInitDetectionExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AprilTagAutonomousInitDetectionExample.java
@@ -32,6 +32,7 @@ import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.firstinspires.ftc.common.Hardware.AprilTagDetectionPipeline;
 
 import java.util.ArrayList;
 
