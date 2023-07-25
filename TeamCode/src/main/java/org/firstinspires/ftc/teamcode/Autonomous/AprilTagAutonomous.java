3ac840f99471662673af25531cd5438a78813d6c
CMP-18996 <calculusgeek11@gmail.com>
2023-07-25 19:13:05 -0400

    added the decimation thingy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AprilTagAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AprilTagAutonomous.java
@@ -1,31 +1,10 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
-/*
- * Copyright (c) 2021 OpenFTC Team
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.openftc.apriltag.AprilTagDetection;
-import org.firstinspires.ftc.common.Hardware.AprilTagDetectionPipeline;
 import org.firstinspires.ftc.common.Hardware.Camera;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -35,16 +14,38 @@ public class AprilTagAutonomous {
     private static final double FEET_PER_METER = 3.28084;
     private int tagIDs[];
     private AprilTagDetection tagOfInterest = null;
+    ArrayList<AprilTagDetection> currentDetections;
+
+    private int numFramesWithoutDetection = 0;
+    private final float DECIMATION_HIGH;
+    private final float DECIMATION_LOW;
+    private final float THRESHOLD_HIGH_DECIMATION_RANGE_METERS;
+    private final int THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION;
 
     public AprilTagAutonomous(Camera camera, int[] tagIDs) {
         this.camera = camera;
         this.tagIDs = tagIDs;
+        DECIMATION_HIGH = 3;
+        DECIMATION_LOW = 2;
+        THRESHOLD_HIGH_DECIMATION_RANGE_METERS = 1.0f;
+        THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION = 4;
+    }
+    public AprilTagAutonomous(Camera camera, int[] tagIDs,
+                              float DECIMATION_HIGH, float DECIMATION_LOW,
+                              float THRESHOLD_HIGH_DECIMATION_RANGE_METERS,
+                              int THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION) {
+        this.camera = camera;
+        this.tagIDs = tagIDs;
+        this.DECIMATION_HIGH = DECIMATION_HIGH;
+        this.DECIMATION_LOW = DECIMATION_LOW;
+        this.THRESHOLD_HIGH_DECIMATION_RANGE_METERS = THRESHOLD_HIGH_DECIMATION_RANGE_METERS;
+        this.THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION = THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION;
     }
     public Camera getCamera() {
         return camera;
     }
-    public int getDetectedTagID() {
-        ArrayList<AprilTagDetection> currentDetections = camera.getAprilTagPipeline().getLatestDetections();
+    public int findTagIDSimple() {
+        currentDetections = camera.getAprilTagPipeline().getLatestDetections();
         if (currentDetections.size() > 0) {
             for (AprilTagDetection tag : currentDetections) {
                 if (Arrays.asList(tagIDs).contains(tag.id)) {
@@ -54,6 +55,21 @@ public class AprilTagAutonomous {
             }
         }
         return -1;
+    }
+    public int findTagIDDecimation() {
+        currentDetections = camera.getAprilTagPipeline().getDetectionsUpdate();
+        if (currentDetections.size() == 0) {
+            numFramesWithoutDetection++;
+            if (numFramesWithoutDetection >= THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION) {
+                camera.getAprilTagPipeline().setDecimation(DECIMATION_LOW);
+            }
+        } else {
+            numFramesWithoutDetection = 0;
+            if (currentDetections.get(0).pose.z < THRESHOLD_HIGH_DECIMATION_RANGE_METERS) {
+                camera.getAprilTagPipeline().setDecimation(DECIMATION_HIGH);
+            }
+        }
+        return findTagIDSimple();
     }
     public double[] returnTagLocalization() {
         Orientation rot = Orientation.getOrientation(tagOfInterest.pose.R, AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES);
