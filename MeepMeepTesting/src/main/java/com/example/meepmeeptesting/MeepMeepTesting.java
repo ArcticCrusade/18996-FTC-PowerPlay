239fb67d4363a7dce6d1000e2110db813b93f4ab
CMP-18996 <calculusgeek11@gmail.com>
2023-08-29 20:52:29 -0400

    added ftclib - WARNING MAY BREAK EVERYTHING

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -13,14 +13,8 @@ public class MeepMeepTesting {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(80, 30, Math.toRadians(180), Math.toRadians(180), 12.33)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(36,-70, Math.toRadians(90)))
-                        .splineToSplineHeading(new Pose2d(36,-30, Math.toRadians(90)), Math.toRadians(90))
-                        .splineToSplineHeading(new Pose2d(-51,-22, Math.toRadians(270)), Math.toRadians(270))
-                        .splineToSplineHeading(new Pose2d(21,15, Math.toRadians(0)), Math.toRadians(0))
-                        .splineToSplineHeading(new Pose2d(-61,-23, Math.toRadians(135)), Math.toRadians(135))
-                        .splineToSplineHeading(new Pose2d(-53,55, Math.toRadians(60)), Math.toRadians(60))
-                        .splineToSplineHeading(new Pose2d(7,-62, Math.toRadians(225)), Math.toRadians(225))
-                        .splineToSplineHeading(new Pose2d(-8,31, Math.toRadians(0)), Math.toRadians(0))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, Math.toRadians(0)))
+                        .splineToSplineHeading(new Pose2d(-27, -1, Math.toRadians(215)), Math.toRadians(215))
                         .build()
                 );
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
