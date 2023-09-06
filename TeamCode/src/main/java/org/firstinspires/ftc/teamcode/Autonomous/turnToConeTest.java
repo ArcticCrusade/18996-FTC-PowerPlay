0f83a54144dccf60fb3064e6afe56e0b28628e70
christopheraustin <arcticplayernoice@gmail.com>
2023-09-05 20:45:09 -0400

    todo updated and turnToConeTest created

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/turnToConeTest.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.Robot;
+import org.firstinspires.ftc.common.Hardware.Camera;
+import org.firstinspires.ftc.common.Hardware.DriveSubsystem;
+import org.firstinspires.ftc.common.Hardware.RobotHardware;
+import org.firstinspires.ftc.common.Commands.TurnToCone;
+
+public class turnToConeTest extends CommandOpMode {
+    RobotHardware robot = RobotHardware.getInstance();
+    DriveSubsystem driveSubsystem;
+    Camera camera;
+
+    @Override
+    public void initialize() {
+        robot.init(hardwareMap, RobotHardware.OpModes.AUTO);
+        driveSubsystem = new DriveSubsystem(robot);
+        camera = new Camera(hardwareMap);
+    }
+
+    @Override
+    public void runOpMode() {
+        CommandScheduler.getInstance().schedule(
+                new TurnToCone(driveSubsystem, camera)
+        );
+    }
+}
