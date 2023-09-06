c8c85050051fb7d7ae7adace5e9d17c504346767
CMP-18996 <calculusgeek11@gmail.com>
2023-09-05 20:59:07 -0400

    yolo

--- TeamCode/src/main/java/org/firstinspires/ftc/common/Commands/AprilTagIDCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/common/Commands/AprilTagIDCommand.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.common.Commands;
 
+import com.acmerobotics.roadrunner.drive.Drive;
 import com.arcrobotics.ftclib.command.CommandBase;
 
 import org.firstinspires.ftc.common.Hardware.Camera;
+import org.firstinspires.ftc.common.Hardware.DriveSubsystem;
 import org.firstinspires.ftc.common.Hardware.RobotHardware;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
@@ -14,19 +16,23 @@ import java.util.ArrayList;
 
 public class AprilTagIDCommand extends CommandBase {
     private Camera camera;
+    private DriveSubsystem drive;
     private static final double FEET_PER_METER = 3.28084;
     private AprilTagDetection tagOfInterest = null;
     ArrayList<AprilTagDetection> currentDetections;
     private double[] stats;
     private final double MARGIN_OF_ERROR = 1.0;
 
-    public AprilTagIDCommand(RobotHardware robot) {
+    public AprilTagIDCommand(RobotHardware robot, DriveSubsystem drive) {
         this.camera = robot.camera;
-        addRequirements(camera);
+        this.drive = drive;
+        addRequirements(this.camera, this.drive);
     }
 
     @Override
-    public void initialize() {
+    public void initialize() {}
+    @Override
+    public void execute() {
         currentDetections = camera.getAprilTagPipeline().getLatestDetections();
         if (currentDetections.size() > 0) {
             for (AprilTagDetection tag : currentDetections) {
@@ -35,9 +41,6 @@ public class AprilTagIDCommand extends CommandBase {
                 }
             }
         }
-    }
-    @Override
-    public void execute() {
         Orientation rot = Orientation.getOrientation(tagOfInterest.pose.R, AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES);
         stats = new double[]{tagOfInterest.pose.x * FEET_PER_METER,
                 tagOfInterest.pose.y * FEET_PER_METER,
@@ -45,9 +48,19 @@ public class AprilTagIDCommand extends CommandBase {
                 rot.firstAngle,
                 rot.secondAngle,
                 rot.thirdAngle};
+        if (tagOfInterest.id == 0) {
+            drive.manualPower(-0.3 * stats[0], -0.3 * Math.abs(stats[1] - 1), 0.01 * stats[3]);
+        } else if (tagOfInterest.id == 1) {
+            drive.manualPower(-0.3 * stats[0], -0.3 * Math.abs(stats[1] - 2), 0.01 * stats[3]);
+        } else if (tagOfInterest.id == 2) {
+            drive.manualPower(-0.3 * stats[0], -0.3 * Math.abs(stats[1] - 3), 0.01 * stats[3]);
+        }
     }
     @Override
     public boolean isFinished() {
-        return true;
+        if (Math.abs(stats[0]) < MARGIN_OF_ERROR && Math.abs(stats[3]) < MARGIN_OF_ERROR) {
+            return true;
+        }
+        return false;
     }
 }
