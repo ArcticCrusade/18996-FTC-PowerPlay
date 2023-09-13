12a0d8bd6b4b4d29eb560ed42ba334b894902301
CMP-18996 <calculusgeek11@gmail.com>
2023-09-12 21:05:41 -0400

    bunch of cone debugging nonsense

--- TeamCode/src/main/java/org/firstinspires/ftc/common/Commands/TurnToCone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/common/Commands/TurnToCone.java
@@ -1,45 +1,88 @@
 package org.firstinspires.ftc.common.Commands;
 
 import com.arcrobotics.ftclib.command.CommandBase;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.common.Hardware.Camera;
 import org.firstinspires.ftc.common.Hardware.DriveSubsystem;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 
 public class TurnToCone extends CommandBase {
-
+    enum Turning {
+        RIGHT,
+        LEFT,
+        FINDING
+    }
+    int increment = 0;
+    Turning turnState = Turning.FINDING;
     private final DriveSubsystem drivetrain;
     private final Camera camera;
-    int centerVal;
-
+    int centerVal = -1;
+    Queue<Integer> centerVals = new LinkedList();
+    Queue<Double> times = new LinkedList();
+    ElapsedTime time = new ElapsedTime();
     public TurnToCone(DriveSubsystem newDrivetrain, Camera newCamera) {
         drivetrain = newDrivetrain;
         camera = newCamera;
         addRequirements(newDrivetrain, drivetrain);
     }
-
+    public Queue<Integer> getCenterVal() {
+        return centerVals;
+    }
+    public Queue<Double> getTimes() {
+        return times;
+    }
+    public boolean foundContour() {
+        return camera.getConePipeline().foundContour;
+    }
     @Override
     public void initialize() {
+        camera.switchPipeline("red");
+        time.reset();
     }
 
     @Override
     public void execute() {
-        camera.switchPipeline("red");
+        times.add(time.milliseconds());
+        if (times.size() >= 11) {
+            times.poll();
+        }
         if (camera.getConePipeline().foundContour) {
             centerVal = camera.getConePipeline().getCenter();
-            if (centerVal < 160) {
-                drivetrain.manualPower(0, 0, .3);
-            } else {
-                drivetrain.manualPower(0, 0, -.3);
+            centerVals.add(centerVal);
+            if (centerVals.size() >= 11) {
+                centerVals.poll();
             }
-        }
-        else {
+            /*if (30 <= centerVal && centerVal <= 38) {
+                if (turnState.equals(Turning.RIGHT)) {
+                    increment++;
+                }
+                turnState = Turning.LEFT;
+                drivetrain.manualPower(0, 0, Math.max(.3 -0.01*increment,0.125));
+            } else {
+                if (turnState.equals(Turning.LEFT)) {
+                    increment++;
+                }
+                turnState = Turning.RIGHT;
+                drivetrain.manualPower(0, 0, Math.max(.3 -0.01*increment,0.125));
+            }*/
+        } else {
             drivetrain.manualPower(0, 0, -.3);
+            increment = 0;
         }
     }
 
     @Override
     public boolean isFinished() {
-        return (centerVal >= 155 && centerVal <= 165);
+        if (30 <= centerVal && centerVal <= 38) {
+            drivetrain.manualPower(0, 0, 0);
+            return true;
+        }
+        return false;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/turnToConeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/turnToConeTest.java
@@ -14,19 +14,26 @@ import org.firstinspires.ftc.common.Commands.TurnToCone;
 public class turnToConeTest extends CommandOpMode {
     RobotHardware robot = RobotHardware.getInstance();
     DriveSubsystem driveSubsystem;
+    TurnToCone t;
     @Override
     public void initialize() {
         CommandScheduler.getInstance().reset();
         robot.init(hardwareMap, RobotHardware.OpModes.AUTO);
         driveSubsystem = new DriveSubsystem(robot);
         register(driveSubsystem, robot.camera);
+        t = new TurnToCone(driveSubsystem, robot.camera);
+        telemetry.addData("Initiated", "true");
+        telemetry.update();
+
+        CommandScheduler.getInstance().schedule(t);
     }
 
     @Override
     public void run() {
         CommandScheduler.getInstance().run();
-        CommandScheduler.getInstance().schedule(
-                new TurnToCone(driveSubsystem, robot.camera)
-        );
+        telemetry.addData("CenterVals", t.getCenterVal());
+        telemetry.addData("Times", t.getTimes());
+        telemetry.addData("Do I see something?", t.foundContour());
+        telemetry.update();
     }
 }
